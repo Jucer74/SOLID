1a8ce8949c8c1655cb5739e9c97ddbcd163a5e3f
Julio Robles <Julio.RoblesU@perficient.com>
2020-10-12 03:46:14 -0500

    OCP Version

---
+++ 2.OCP-Open-Closed Principle/2.2.Solid.Principles.OCP/ReportTAB.cs
@@ -0,0 +1,24 @@
+﻿namespace Solid.Principles
+{
+  using System.Collections.Generic;
+  using System.IO;
+  using Define;
+  using Dto;
+
+  public class ReportTAB : IReportGenerator
+  {
+    public void Generate(string reportFilename, List<EmployeeDto> employees)
+    {
+      var fullReportFileName = $"{Constants.ReportsPath}{reportFilename}.tab";
+      var sw = new StreamWriter(fullReportFileName);
+
+      foreach (var emp in employees)
+      {
+        sw.WriteLine($"{emp.Id}\t{emp.FirstName}\t{emp.LastName}\t{emp.HireDate}\t{emp.Email}\t{emp.Phone}");
+      }
+
+      sw.Flush();
+      sw.Close();
+    }
+  }
+}
